4c3614f7200f5da9e34ee3f72fa80d461156ce03
Stephen Mc Guinness <stephen.mcguinness@digitgaming.com>
2021-09-02 10:36:19 +0100

    Updates the Alpine image version, uses python3 and removes AWS credential support

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM alpine:3.3
+FROM alpine:3.14.2
 
-RUN apk add --update-cache python py-pip ca-certificates tzdata && pip install s3cmd
+RUN apk add --update-cache python3 py-pip ca-certificates tzdata && pip install s3cmd
 RUN apk add --update bash && apk add --update mysql-client && rm -rf /var/cache/apk/*
 
 ENV MYSQLDUMP_OPTIONS --extended-insert --replace --compress
@@ -10,8 +10,3 @@ ENV AWS_PATH None
 ADD backup.sh /backup.sh
 ADD s3cfg /root/.s3cfg
 RUN chmod +x /backup.sh
-ARG AWS_ACCESS_KEY_ID=""
-ARG AWS_SECRET_ACCESS_KEY=""
-
-WORKDIR /root/.aws/
-RUN echo "[Credentials]\naws_access_key_id = $AWS_ACCESS_KEY_ID\naws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> credentials
--- backup.sh
+++ backup.sh
@@ -37,13 +37,8 @@ HOST_PARAMS="-h $MYSQL_HOST --port $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD"
 
 echo "[mysqlbackup] Beginning mysqldump of ${MYSQL_HOST}/${MYSQL_DATABASE}"
 
-if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-    echo "[mysqlbackup] Not using AWS credentials with s3cmd"
-    mysqldump $HOST_PARAMS $MYSQLDUMP_OPTIONS $MYSQL_DATABASE | gzip | s3cmd put - s3://$AWS_PATH/$MYSQL_DATABASE-$(date +"%Y%m%d%H%M%S").sql.gz
-else
-    echo "[mysqlbackup] Using AWS credentials as parameters for s3cmd"
-    mysqldump $HOST_PARAMS $MYSQLDUMP_OPTIONS $MYSQL_DATABASE | gzip | s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY put - s3://$AWS_PATH/$MYSQL_DATABASE-$(date +"%Y%m%d%H%M%S").sql.gz
-fi
+mysqldump $HOST_PARAMS $MYSQLDUMP_OPTIONS $MYSQL_DATABASE | gzip | s3cmd put - s3://$AWS_PATH/$MYSQL_DATABASE-$(date +"%Y%m%d%H%M%S").sql.gz
+
 echo "[mysqlbackup] Finished"
 
 exit 0
